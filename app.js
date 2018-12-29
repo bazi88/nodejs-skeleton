0a566d001c537089b1453bb91193a693a6a232bb
quan.nn <nguyenquan.trade@gmail.com>
2018-12-29 10:38:48 -0800

    add

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var db = require('./db/db');
 var index = require('./routes/index');
+var user = require('./routes/user');
 
 var app = express();
 app.use('/', index);
+app.use('/user',user);
 
 module.exports = app;
---
+++ models/user.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema(
+    {
+        email: String,
+        password: String,
+        name: String
+    }
+  );
+module.exports = mongoose.model('User', UserSchema);
