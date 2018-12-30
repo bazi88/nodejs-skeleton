164177624055f9f4775d06a0b6354bd9e0146eb8
quan.nn <nguyenquan.trade@gmail.com>
2018-12-30 06:24:38 -0800

    first commit

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var db = require('./db/db');
 var index = require('./routes/index');
 var user = require('./routes/user');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 var app = express();
 app.use('/', index);
 app.use('/user',user);
